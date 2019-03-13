4c5b51b5dbcabda89a2927c82ebb4fca52b2492b
Tauheed Elahee <36247967+Tauheed-Elahee@users.noreply.github.com>
2019-03-12 22:11:46 -0400

    Update Drinking-Machine-Arduino-Code.ino
    Test

--- Drinking-Machine-Arduino-Code.ino
+++ Drinking-Machine-Arduino-Code.ino
@@ -1,9 +1,18 @@
+#inculde <pinLayout.h>
+#include <LCD.h>
+
+//Global Variables
+LCD lcd;
+
 void setup() {
   // put your setup code here, to run once:
+  pinLayout pin;
+  pin.setupPins();
+  LCD lcd;
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+    lcd.printHomeScreen();
 }
