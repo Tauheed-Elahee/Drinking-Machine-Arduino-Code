fa8257bc83d655000308a6bc08adf958a0663117
Tauheed Elahee <36247967+Tauheed-Elahee@users.noreply.github.com>
2019-03-14 12:39:39 -0400

    Add files via upload
    Upload PinLayout library.

---
+++ libraries/PinLayout/PinLayout.h
@@ -0,0 +1,20 @@
+/*
+
+This file includes all the constants mapped to the Arduino Pins
+
+ */
+
+
+#ifndef PinLayout_h
+#define PinLayout_h
+
+#include "Arduino.h"
+
+class PinLayout {
+	public:
+		PinLayout();
+		void setup();
+	private:
+};
+
+#endif
