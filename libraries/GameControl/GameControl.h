1e8d86276c8a6fb94c03367280fbd1d77e9bae8c
Rishi Sharma <contact@rishisharma.io>
2019-03-26 10:23:26 -0400

    Deleted defines in GameControl headers

--- libraries/GameControl/GameControl.h
+++ libraries/GameControl/GameControl.h
@@ -4,15 +4,6 @@
 #ifndef game_Control_h
 #define game_Control_h
 
-#define plinkoEnable 22
-#define bin1 23
-#define bin2 24
-#define bin3 25
-#define bin4 26
-#define bin5 27
-#define bin6 28
-#define bin7 29
-
 class GameControl
 {
   public:
