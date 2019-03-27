211e0c67be81bdbd14f567d68e82a043f5a8aa92
Rishi Sharma <contact@rishisharma.io>
2019-03-26 20:09:41 -0400

    Preliminary bluetooth functionality

--- Drinking-Machine-Arduino-Code.ino
+++ Drinking-Machine-Arduino-Code.ino
@@ -1,7 +1,12 @@
-
 #include "LCD.h"
 #include "PumpControl.h"
 #include "GameControl.h"
+
+LCD lcd;
+GameControl game;
+PumpControl pumps;
+
+unsigned long int refTime;
 
 enum stateMachine {
   START_STATE,
@@ -10,24 +15,34 @@ enum stateMachine {
   GAME_STATE,
   BLUETOOTH_SELECT_PRIMARY_MENU,
   BLUETOOTH_SELECT_DRINK_MENU,
+  BLUETOOTH_RECIPE_STATE
 };
 
-LCD lcd;
-GameControl game;
-
-enum stateMachine currentState = START_STATE;
-
+enum stateMachine currentState;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  currentState = START_STATE;
 }
 
 enum stateMachine startState () {
-  
+  delay(1000);
+  lcd.printWelcomeScreen();
+  flushSerial1();
+  // the "select" button will initiate manual mode
+  while (true) {
+    if (digitalRead(33)) {
+      return MANUAL_SELECT_PRIMARY_MENU;
+    }
+    if (Serial1.available() > 0) {
+      flushSerial1();
+      return BLUETOOTH_SELECT_PRIMARY_MENU;
+    }
+  }
 }
 
 enum stateMachine manualSelectPrimaryMenu () {
-  
 }
 
 enum stateMachine manualSelectDrinkMenu () {
@@ -35,10 +50,41 @@ enum stateMachine manualSelectDrinkMenu () {
 }
 
 enum stateMachine bluetoothSelectPrimaryMenu () {
-  
+  lcd.printBluetoothScreen();
+  // millisecond timeout
+  char t;
+  int counter = 60;
+  refTime = millis();
+  lcd.printTimeoutPlaceholder();
+  while (counter) {
+    if (oneSecondTick(refTime)) {
+      counter--;
+      refTime = millis();
+      lcd.updateTimeout(counter);
+    }
+    if (Serial1.available() > 0) {
+      t = Serial1.read();
+      if (t == 'a'){
+        return BLUETOOTH_SELECT_DRINK_MENU;
+      }
+      if (t == 'b') {
+        return GAME_STATE;
+      }
+      if (t == 'c') {
+        return BLUETOOTH_RECIPE_STATE;
+      }
+      if (t == 'e') {
+        return START_STATE;
+      }
+      flushSerial1();
+    }
+  } return START_STATE;
 }
 
 enum stateMachine bluetoothSelectDrinkMenu () {
+}
+
+enum stateMachine bluetoothRecipeSaveState () {
   
 }
 
@@ -63,8 +109,22 @@ void loop() {
     case BLUETOOTH_SELECT_DRINK_MENU:
       currentState = bluetoothSelectDrinkMenu();
       break;
+    case BLUETOOTH_RECIPE_STATE:
+      currentState = bluetoothRecipeSaveState();
+      break;
     case GAME_STATE:
       currentState = gameState();
       break;
   }
+}
+
+void flushSerial1() {
+  char t;
+  while (Serial1.available() > 0 ) {
+   t = Serial1.read();
+  }
+}
+
+bool oneSecondTick (unsigned long int &lastCheck){
+  return (millis() - lastCheck) > 1000;
 }
